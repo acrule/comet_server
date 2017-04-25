ebb6bb8f509b36e24fa32adcf1647d961c1c8365
acrule <acrule@gmail.com>
2017-04-25 16:01:01 -0700

    first pass at history flow using d3

--- comet_viewer.py
+++ comet_viewer.py
@@ -5,29 +5,13 @@ Comet Server: Server extension paired with nbextension to track notebook use
 import os
 import cgi
 import nbconvert
+import nbformat
 
 def get_viewer_html(data_dir):
-    html = '<html><head><style>\
-            body{ width: 20000px;}\
-            .wrap{ \
-                width: 320px; height: 4000px;\
-                padding: 0;\
-                overflow: hidden;\
-                float: left;}\
-            .frame { width: 1280px;\
-                height: 12000px;\
-                border: 1px solid black}\
-            .frame { \
-                -moz-transform: scale(0.25); \
-                -moz-transform-origin: 0 0; \
-                -o-transform: scale(0.25); \
-                -o-transform-origin: 0 0; \
-                -webkit-transform: scale(0.25); \
-                -webkit-transform-origin: 0 0;} \
-            </style></head><body>'
-
+    
+    data = []
     version_dir = os.path.join(data_dir, 'versions')
-
+    
     if os.path.isdir(version_dir):
         versions = [f for f in os.listdir(version_dir)
             if os.path.isfile(os.path.join(version_dir, f))
@@ -35,19 +19,50 @@ def get_viewer_html(data_dir):
 
         for v in versions:
             nb_path = os.path.join(version_dir, v)
-            html_exporter = nbconvert.HTMLExporter()
-            (body, resources) = html_exporter.from_file(nb_path)
-            escaped_html = cgi.escape(body, True)
-
-            frame = '<div class="wrap"> \
-                        <iframe class="frame" srcdoc=\"%s\">\
-                        </iframe>\
-                    </div>' % escaped_html
-            html = html + frame
-
-        html = html + '</body></html>'
+            nb_cells = nbformat.read(nb_path, nbformat.NO_CONVERT)['cells']
+            cell_types = []
+            for c in nb_cells:
+                cell_types.append(c.cell_type)
+            data.append(cell_types)
+        
+        html = """<!DOCTYPE html>\n
+            <html>\n
+            <body>\n
+            <script src="https://d3js.org/d3.v4.min.js"></script>\n
+            <script>\n
+            var width = 960;\n
+            var height = 600;\n
+            var cellSize = 16;\n
+            \n
+            var data = """ + str(data) + """\n
+            \n
+            var svg = d3.select("body")\n
+                .append("svg")\n
+                .attr("width", width)\n
+                .attr("height", height)\n
+            \n
+            var nb = svg.selectAll("g")\n
+                .data(data)\n
+                .enter().append("g")\n
+            \n
+            nb.each(function(p, j) {\n
+                d3.select(this)\n
+                .selectAll("rect")\n
+                    .data(function(d){return d; })\n
+                    .enter().append("rect")\n
+                    .attr("width", cellSize)\n
+                    .attr("height", cellSize)\n
+                    .attr("x", function(d, i){ return j*cellSize; })\n
+                    .attr("y", function(d, i) { return i * cellSize; })\n
+                    .attr("fill", function(d) { if(d == "markdown"){return "steelblue";} else{return "gray"} })\n
+                    .attr("stroke", "white");\n
+            });\n
+            \n
+            </script>\n
+            </body>\n
+            </html>"""  
     
     else:
-        html = '<h1>No Data</h1> <p>There is no data saved for %s</p>' % path    
+        html = '<h1>No Data</h1> <p>There is no data saved for %s</p>' % data_dir    
         
     return html
